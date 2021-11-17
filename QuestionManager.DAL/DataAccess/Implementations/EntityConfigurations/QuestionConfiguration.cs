f468435a0bab0896f47e789f88b031593bf0ad58
anton <anton@DESKTOP-8NE0IIK>
2021-11-17 19:35:20 +0200

    Added Configurations

---
+++ QuestionManager.DAL/DataAccess/Implementations/EntityConfigurations/QuestionConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QuestionManager.DAL.DataAccess.Implementations.Entities;
+
+namespace QuestionManager.DAL.DataAccess.Implementations.EntityConfigurations
+{
+    public class QuestionConfiguration : IEntityTypeConfiguration<QuestionEntity>
+    {
+        public void Configure(EntityTypeBuilder<QuestionEntity> builder)
+        {
+            builder.ToTable("Question").HasKey(p => p.Id);
+            builder.Property(p => p.Question).IsRequired().HasColumnName("Question").HasColumnType("varchar").HasMaxLength(100);
+            builder.Property(p => p.Correct).IsRequired().HasColumnName("Correct").HasColumnType("bit");
+            builder.Property(p => p.Complexity).IsRequired().HasColumnName("Complexity").HasColumnType("int");
+        }
+    }
+}
---
+++ QuestionManager.DAL/DataAccess/Implementations/EntityConfigurations/UserConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QuestionManager.DAL.DataAccess.Implementations.Entities;
+
+namespace QuestionManager.DAL.DataAccess.Implementations.EntityConfigurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<UserEntity>
+    {
+        public void Configure(EntityTypeBuilder<UserEntity> builder)
+        {
+            builder.ToTable("User").HasKey(p => p.Id);
+            builder.Property(p => p.Email).IsRequired().HasColumnName("Email").HasColumnType("varchar").HasMaxLength(40);
+            builder.Property(p => p.Score).IsRequired().HasColumnName("Score").HasColumnType("int");
+        }
+    }
+}
