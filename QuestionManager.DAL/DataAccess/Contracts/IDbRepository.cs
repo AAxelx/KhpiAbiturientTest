36c896b70bbeee94e2144b1f1a873365f5f7cd21
anton <anton@DESKTOP-8NE0IIK>
2021-11-17 19:34:48 +0200

    Added Contracts for entities and DbRepository

---
+++ QuestionManager.DAL/DataAccess/Contracts/IDbRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace QuestionManager.DAL.DataAccess.Contracts
+{
+    public interface IDbRepository
+    {
+        Task<IEnumerable<T>> GetAll<T>()
+           where T : class, IEntity;
+        Task<T> GetById<T>(Guid id)
+            where T : class, IEntity;
+        Task<T> AddAsync<T>(T newEntity)
+            where T : class, IEntity;
+        Task<bool> UpdateAsync<T>(T entity)
+            where T : class, IEntity;
+        Task<bool> RemoveAsync<T>(Guid id)
+            where T : class, IEntity;
+    }
+}
---
+++ QuestionManager.DAL/DataAccess/Contracts/IEntity.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace QuestionManager.DAL.DataAccess.Contracts
+{
+    public interface IEntity
+    {
+        public Guid Id { get; set; }
+    }
+}
