4d3f9efa60fa84907004fed92647c4142411e003
anton <anton@DESKTOP-8NE0IIK>
2021-11-23 13:57:32 +0200

    Added requests

---
+++ QuestionManager.BLL/Models/Requests/AddRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace QuestionManager.BLL.Models.Requests
+{
+    public class AddRequest
+    {
+        public string Question { get; set; }
+        public string Answear { get; set; }
+        public int Complexity { get; set; }
+        public string SecondOption { get; set; }
+        public string ThirdOption { get; set; }
+    }
+}
---
+++ QuestionManager.BLL/Models/Requests/CalculatePointsRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace QuestionManager.BLL.Models.Requests
+{
+    public class CalculatePointsRequest
+    {
+        public List<AnswearModel> Questions { get; set; }
+        public string Email { get; set; }
+    }
+}
