5e3c9c6f17831b75827c96199f320c0f3e95e491
anton <anton@DESKTOP-8NE0IIK>
2021-11-23 12:28:41 +0200

    Updated QuestionTable

---
+++ QuestionManager.DAL/Migrations/20211123101637_InitialCreate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QuestionManager.DAL.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Question",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Question = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Answear = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Complexity = table.Column<int>(type: "int", nullable: false),
+                    SecondOption = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ThirdOption = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Question", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Email = table.Column<string>(type: "varchar(40)", maxLength: 40, nullable: false),
+                    Score = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Question");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
