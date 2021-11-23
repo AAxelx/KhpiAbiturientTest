b31e2688021a08e94dc7662c75ae1b54e5c3c7c9
anton <anton@DESKTOP-8NE0IIK>
2021-11-23 13:58:56 +0200

    Added abstraction for service

---
+++ QuestionManager.BLL/Services/Abstractions/IQuestionService.cs
@@ -0,0 +1,16 @@
+﻿using QuestionManager.BLL.Models;
+using QuestionManager.BLL.Models.Responses.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace QuestionManager.BLL.Services.Abstractions
+{
+    public interface IQuestionService
+    {
+        Task<IServiceResponse> GetAllAsync();
+        Task<IServiceResponse> AddAsync(string question, string answear, int complexity, string secondOption, string thirdOption);
+        Task<IServiceResponse> DeleteAsync(Guid questionId);
+        Task<IServiceResponse> CalculatePointsAsync(List<AnswearModel> questions, string email);
+    }
+}
