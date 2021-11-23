3600c9584f4c31785f0068a82ea985d4479681b7
anton <anton@DESKTOP-8NE0IIK>
2021-11-23 13:57:18 +0200

    Added responses

---
+++ QuestionManager.BLL/Models/Responses/CalculatePointsResponse.cs
@@ -0,0 +1,10 @@
+﻿using QuestionManager.BLL.Models.Responses.Abstractions;
+
+namespace QuestionManager.BLL.Models.Responses
+{
+    public class CalculatePointsResponse : IServiceResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ QuestionManager.BLL/Models/Responses/GetAllResponse.cs
@@ -0,0 +1,13 @@
+﻿using QuestionManager.BLL.Models.Responses.Abstractions;
+using System.Collections.Generic;
+
+namespace QuestionManager.BLL.Models.Responses
+{
+    public class GetAllResponse : IServiceResponse
+    {
+        public List<QuestionModel> EasyQuestions { get; set; }
+        public List<QuestionModel> MediumQuestions { get; set; }
+        public List<QuestionModel> HardQuestions { get; set; }
+        public string Message { get; set; }
+    }
+}
