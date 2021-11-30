7658c71ed019f31cb9bb7489c526e484defef419
anton <anton@DESKTOP-8NE0IIK>
2021-11-30 17:34:08 +0200

    Added UserController

---
+++ QuestionManager/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuestionManager.BLL.Services.Abstractions;
+using System.Threading.Tasks;
+
+namespace QuestionManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetByEmail(string email)
+        {
+            var response = await _userService.GetByEmailAsync(email);
+
+            if(response.Message == null)
+            {
+                return Ok(response);
+            }
+
+            return BadRequest(response.Message);
+        }
+    }
+}
